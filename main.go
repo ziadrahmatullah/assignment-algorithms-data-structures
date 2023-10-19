cc6c18643687d16c4d794d7d153547b926213bd3
Muhammad Ziad Rahmatullah <ziad.rahmatullah@shopee.com>
2023-10-19 23:06:40 +0700

    feat: add rotateImage for image

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
-import "fmt"
+// import "fmt"
 
 // If you want to test your code, you may test it here...
 func main() {
@@ -7,15 +7,15 @@ func main() {
 	// _, _ := LastDayInJail(nil, "")
 
 	// Task 2.a
-	result := RotateImage([][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	})
-	fmt.Print(result)
+	// result := RotateImage([][]int{
+	// 	{1, 2, 3},
+	// 	{4, 5, 6},
+	// 	{7, 8, 9},
+	// })
+	// fmt.Print(result)
 
 	// Task 2.b
-	// RunRotateActualImage()
+	RunRotateActualImage()
 
 	// Task 3.a
 	// string := RobotTranslatorV2("LLRRX XX")
--- rotateimage.go
+++ rotateimage.go
@@ -39,7 +39,20 @@ func RotateImage(pixels [][]int) [][]int {
 func RotateActualImage(pixels [][]Pixel) [][]Pixel {
 	// Write your code here
 	// --------------------
-	return nil
+	rows:= len(pixels)
+	cols := len(pixels[0])
+	result := make([][]Pixel, rows)
+	for i:=0; i< len(result); i++{
+		result[i] = make([]Pixel, cols)
+	}
+	k := cols-1;
+	for i:=0; i<rows; i++{
+		for j:= 0;j<cols; j++{
+			result[j][k] = pixels[i][j]
+		}
+		k--;
+	}
+	return result
 	// --------------------
 }
 
